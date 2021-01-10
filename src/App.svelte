2b8337efb41ad9929244dd923673b9210e8a06ab
Rodrigo Couto <r@rodg.co>
2021-01-10 11:50:20 -0300

    installed storybook, downgraded tailwind + postcss to avoid conflict of postcss-8

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,18 @@
 <script>
   import "./main.css";
 
-  import { timer } from "./timer";
-  import { interval } from "rxjs";
-  import { map, startWith } from "rxjs/operators";
-
-  const rxTimer = interval(1000).pipe(
-    startWith(0),
-    map((v) => v * 2)
-  );
+  import Page from "./stories/Page.svelte";
 </script>
 
 <!-- App.svelte -->
-<style>
+<style lang="scss">
   :global(body) {
     font-family: Arial, Helvetica, sans-serif;
     font-size: 5rem;
     height: 100vh;
     display: flex;
-    background: azure;
-  }
-  .app {
-    display: grid;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-  }
-  .box {
-    margin: 0 auto;
+    @apply bg-red-200;
   }
 </style>
 
-<div class="app">
-  <div class="px-5 bg-green-300 box">
-    <div>TS Timer {$timer}</div>
-    <div>Rx Timer {$rxTimer}</div>
-  </div>
-</div>
+<Page />
